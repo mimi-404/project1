7287a277d3941af8fe94f68e17c909aa4b93cdd5
aayushaaaa <aayushaaaa.i@gmail.com>
2024-05-11 16:52:13 +0545

    Revert "here"
    This reverts commit 9bfd0ef1c5f6218b8d316bbe619fff52908ab776.

--- My project/Assets/Scripts/Environment Generator.cs
+++ My project/Assets/Scripts/Environment Generator.cs
@@ -39,3 +39,56 @@ public class EnvironmentGenerator : MonoBehaviour
         _spriteShapeController.spline.InsertPointAt(_levelLength + 1, new Vector3(transform.position.x, transform.position.y - _bottom));
     }
 }
+
+public class EnvironmentGenerator : MonoBehaviour
+{
+    [SerializeField] private SpriteShapeController _spriteShapeController;
+
+    [SerializeField, Range(3f, 100f)] private int _levelLength = 50;
+    [SerializeField, Range(1f, 50f)] private float _xMultiplier = 2f;
+    [SerializeField, Range(1f, 50f)] private float _yMultiplier = 2f;
+    [SerializeField, Range(0f, 1f)] private float _curveSmoothness = 0.5f;
+    [SerializeField] private float _noiseStep = 0.5f;
+    [SerializeField] private float _bottom = 10f;
+
+    private Vector3 _lastPos;
+    private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
+    private Vector3 _initialScale;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
+        _initialScale = transform.localScale;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = _initialPosition;
+        transform.rotation = _initialRotation;
+        transform.localScale = _initialScale;
+    }
+
+    private void OnValidate()
+    {
+        _spriteShapeController.spline.Clear(); // Corrected here
+
+        for (int i = 0; i < _levelLength; i++)
+        {
+            _lastPos = transform.position + new Vector3(i * _xMultiplier, Mathf.PerlinNoise(0, i * _noiseStep) * _yMultiplier);
+            _spriteShapeController.spline.InsertPointAt(i, _lastPos);
+
+            if (i != 0 && i != _levelLength - 1)
+            {
+                _spriteShapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+                _spriteShapeController.spline.SetLeftTangent(i, Vector3.left * _xMultiplier * _curveSmoothness);
+                _spriteShapeController.spline.SetRightTangent(i, Vector3.right * _xMultiplier * _curveSmoothness);
+            }
+        }
+
+        _spriteShapeController.spline.InsertPointAt(_levelLength, new Vector3(_lastPos.x, transform.position.y - _bottom));
+
+        _spriteShapeController.spline.InsertPointAt(_levelLength + 1, new Vector3(transform.position.x, transform.position.y - _bottom));
+    }
+}
