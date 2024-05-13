05439d6854f0c1e296ad2a954bd03f73cf6f32e8
aayushaaaa <aayushaaaa.i@gmail.com>
2024-05-13 14:09:10 +0545

    bg tansition

--- My project/Assets/Free 2D Cartoon Parallax Background/Demo/Script/BackgroundControl_0.cs
+++ My project/Assets/Free 2D Cartoon Parallax Background/Demo/Script/BackgroundControl_0.cs
@@ -1,3 +1,53 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class BackgroundControl_0 : MonoBehaviour
+// {
+//     [Header("BackgroundNum 0 -> 3")]
+//     public int backgroundNum;
+//     public Sprite[] Layer_Sprites;
+//     private GameObject[] Layer_Object = new GameObject[5];
+//     private int max_backgroundNum = 3;
+//     public float transitionInterval = 5.0f; // Time in seconds between transitions
+
+//     void Start()
+//     {
+//         for (int i = 0; i < Layer_Object.Length; i++){
+//             Layer_Object[i] = GameObject.Find("Layer_" + i);
+//         }
+        
+//         ChangeSprite();
+//         StartCoroutine(AutoTransition());
+//     }
+
+//     void ChangeSprite(){
+//         Layer_Object[0].GetComponent<SpriteRenderer>().sprite = Layer_Sprites[backgroundNum*5];
+//         for (int i = 1; i < Layer_Object.Length; i++){
+//             Sprite changeSprite = Layer_Sprites[backgroundNum*5 + i];
+//             //Change Layer_1->7
+//             Layer_Object[i].GetComponent<SpriteRenderer>().sprite = changeSprite;
+//             //Change "Layer_(*)x" sprites in children of Layer_1->7
+//             Layer_Object[i].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = changeSprite;
+//             Layer_Object[i].transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = changeSprite;
+//         }
+//     }
+
+//     IEnumerator AutoTransition()
+//     {
+//         while (true)
+//         {
+//             yield return new WaitForSeconds(transitionInterval);
+//             NextBG();
+//         }
+//     }
+
+//     public void NextBG(){
+//         backgroundNum = backgroundNum + 1;
+//         if (backgroundNum > max_backgroundNum) backgroundNum = 0;
+//         ChangeSprite();
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +59,8 @@ public class BackgroundControl_0 : MonoBehaviour
     public Sprite[] Layer_Sprites;
     private GameObject[] Layer_Object = new GameObject[5];
     private int max_backgroundNum = 3;
+    public float transitionInterval = 5.0f; // Time in seconds between transitions
+
     void Start()
     {
         for (int i = 0; i < Layer_Object.Length; i++){
@@ -16,12 +68,7 @@ public class BackgroundControl_0 : MonoBehaviour
         }
         
         ChangeSprite();
-    }
-
-    void Update() {
-        //for presentation without UIs
-        if (Input.GetKeyDown(KeyCode.RightArrow)) NextBG();
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) BackBG();
+        StartCoroutine(AutoTransition());
     }
 
     void ChangeSprite(){
@@ -36,14 +83,18 @@ public class BackgroundControl_0 : MonoBehaviour
         }
     }
 
+    IEnumerator AutoTransition()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(transitionInterval);
+            NextBG();
+        }
+    }
+
     public void NextBG(){
         backgroundNum = backgroundNum + 1;
         if (backgroundNum > max_backgroundNum) backgroundNum = 0;
-        ChangeSprite();
-    }
-    public void BackBG(){
-        backgroundNum = backgroundNum - 1;
-        if (backgroundNum < 0) backgroundNum = max_backgroundNum;
         ChangeSprite();
     }
 }
