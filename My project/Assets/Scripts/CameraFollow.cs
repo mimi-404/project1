d6aa0197eed8c92496fe904db03cf652085459a5
aayusha <aayushaaaa.i@gmail.com>
2024-05-14 21:50:08 +0545

    camerafollow script

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -1,34 +1,34 @@
-// using System.Collections;
-// using System.Collections.Generic;
+// // using System.Collections;
+// // using System.Collections.Generic;
+// // using UnityEngine;
+
+// // public class NewBehaviourScript : MonoBehaviour
+// // {
+// //     // Start is called before the first frame update
+// //     void Start()
+// //     {
+        
+// //     }
+
+// //     // Update is called once per frame
+// //     void Update()
+// //     {
+        
+// //     }
+// // }
 // using UnityEngine;
 
-// public class NewBehaviourScript : MonoBehaviour
+// public class CameraFollow : MonoBehaviour
 // {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
+//     [SerializeField] private Transform Layer_0; // Assign this in the inspector
+//     [SerializeField] private Vector3 offset; // You can adjust this in the inspector
 
-//     // Update is called once per frame
-//     void Update()
+//     private void Update()
 //     {
-        
+//         if (Layer_0 != null)
+//         {
+//             Vector3 newPosition = new Vector3(Layer_0.position.x + offset.x, transform.position.y, transform.position.z);
+//             transform.position = newPosition;
+//         }
 //     }
 // }
-using UnityEngine;
-
-public class CameraFollow : MonoBehaviour
-{
-    [SerializeField] private Transform Layer_0; // Assign this in the inspector
-    [SerializeField] private Vector3 offset; // You can adjust this in the inspector
-
-    private void Update()
-    {
-        if (Layer_0 != null)
-        {
-            Vector3 newPosition = new Vector3(Layer_0.position.x + offset.x, transform.position.y, transform.position.z);
-            transform.position = newPosition;
-        }
-    }
-}
