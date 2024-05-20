868c7d5056d3f133a52694afd0f854dde47dc60c
mimi-404 <sm03231722@student.ku.edu.np>
2024-05-21 05:31:39 +0545

    fuel canister fixed

--- My project/Assets/Scripts/fuelcanister.cs
+++ My project/Assets/Scripts/fuelcanister.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fuelCanister : MonoBehaviour
+public class FuelCanister : MonoBehaviour
 {
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.CompareTag("Player"))
         {
-            fuelcontroller.instance.FillFuel();
+           
+            if (fuelcontroller.instance != null)
+            {
+                fuelcontroller.instance.FillFuel();
+            }
+            else
+            {
+                Debug.LogError("FuelController instance is not set.");
+            }
+
+            
             Destroy(gameObject);
         }
     }
-    
-
 }
