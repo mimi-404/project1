bb18242dc784f2a266c8f2ebd798c1b66b24dff9
aayushaaaa <aayushaaaa.i@gmail.com>
2024-05-25 12:23:00 +0545

    f

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -3,37 +3,33 @@
 
 // public class CameraFollow : MonoBehaviour
 // {
-//     [SerializeField] private Transform Layer_0; // Assign this in the inspector
-//     [SerializeField] private Vector3 offset; // You can adjust this in the inspector
+//     public Transform target;
+//     public DriveCar vehicle;
+ 
+//     public Vector3 offset = new Vector3(10f, 0f, 0f);
 
-//     private void Update()
-//     {
-//         if (Layer_0 != null)
+//     private void LateUpdate()
+//     {   
+//         if (target == null)
 //         {
-//             Vector3 newPosition = new Vector3(Layer_0.position.x + offset.x, transform.position.y, transform.position.z);
-//             transform.position = newPosition;
+//             Debug.LogError("Target is not set");
+//             return;
 //         }
+//         float smoothSpeed = vehicle.GetSpeed() * 1.2f;
+//         Vector3 desiredPosition = target.position + offset;
+//         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+//         transform.position = smoothedPosition;
 //     }
 // }
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class FollowCar : MonoBehaviour
 {
-    public Transform target;
-    public DriveCar vehicle;
- 
-    public Vector3 offset = new Vector3(10f, 0f, 0f);
+    public Transform carTransform;
+    public Vector3 offset;
 
-    private void LateUpdate()
-    {   
-        if (target == null)
-        {
-            Debug.LogError("Target is not set");
-            return;
-        }
-        float smoothSpeed = vehicle.GetSpeed() * 1.2f;
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+    private void Update()
+    {
+        transform.position = new Vector3(carTransform.position.x + offset.x, carTransform.position.y + offset.y, offset.z);
     }
 }
