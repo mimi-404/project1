30026a5da6c2afb1a60ab2b9c15e0f67906c631c
mimi-404 <sm03231722@student.ku.edu.np>
2024-05-25 14:06:22 +0545

    Reapply "fuel gradient fixed"
    This reverts commit 129d0014effd6e637130facdd0cf6f544d4a504b.

--- My project/Assets/Scripts/Parallax.cs
+++ My project/Assets/Scripts/Parallax.cs
@@ -1,46 +1,25 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public class ParallaxEffect : MonoBehaviour
 {
-    public bool Camera_Move;
-    public float Camera_MoveSpeed = 1.5f;
-    [Header("Layer Setting")]
-    public float[] Layer_Speed = new float[5];
-    public GameObject[] Layer_Objects = new GameObject[7];
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEffect;
 
-    private Transform _camera;
-    private float[] startPos = new float[5];
-    private float boundSizeX;
-    private float sizeX;
-    private GameObject Layer_0;
     void Start()
     {
-        _camera = Camera.main.transform;
-        sizeX = Layer_Objects[0].transform.localScale.x;
-        boundSizeX = Layer_Objects[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-        for (int i=0;i<5;i++){
-            startPos[i] = _camera.position.x;
-        }
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    void Update(){
-        //Moving camera
-        if (Camera_Move){
-        _camera.position += Vector3.right * Time.deltaTime * Camera_MoveSpeed;
-        }
-        for (int i=0;i<5;i++){
-            float temp = (_camera.position.x * (1-Layer_Speed[i]) );
-            float distance = _camera.position.x  * Layer_Speed[i];
-            Layer_Objects[i].transform.position = new Vector2 (startPos[i] + distance, _camera.position.y);
-            if (temp > startPos[i] + boundSizeX*sizeX){
-                startPos[i] += boundSizeX*sizeX;
-            }else if(temp < startPos[i] - boundSizeX*sizeX){
-                startPos[i] -= boundSizeX*sizeX;
-            }
-            
-        }
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
     }
 }
