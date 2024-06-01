ad2d7b401d5bd4bb1668f2f1c2cfcdf229fc601e
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-01 22:30:35 +0545

    game over

--- My project/Assets/Scripts/driverdeathfromhead.cs
+++ My project/Assets/Scripts/driverdeathfromhead.cs
@@ -1,14 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class driverdeathfromhead : MonoBehaviour
+//{
+//   private void OnCollisiondeath(Collision2D collision)
+//    {
+//        if (collision.gameObject.CompareTag("ground"))
+//        {
+//            gamemanager.instance.GameOver();
+//        }
+//    }
+//}
 using UnityEngine;
 
 public class driverdeathfromhead : MonoBehaviour
 {
-   private void OnCollisiondeath(Collision2D collision)
+    private gamemanager gameOverManager;
+
+    private void Start()
     {
-        if (collision.gameObject.CompareTag("ground"))
+        // Find the GameOverManager in the scene (ensure it exists)
+        gameOverManager = FindObjectOfType<gamemanager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check for collision with any object
+        if (collision.collider != null)
         {
-            gamemanager.instance.GameOver();
+            // Trigger the game over logic
+            gameOverManager.GameOver();
         }
     }
 }
