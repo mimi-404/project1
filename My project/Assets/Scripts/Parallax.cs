0bd3043409ae86dfed899705fe714cd0f789894f
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-03 13:52:13 +0545

    smooth parallax bg?

--- My project/Assets/Scripts/Parallax.cs
+++ My project/Assets/Scripts/Parallax.cs
@@ -1,25 +1,64 @@
+// using UnityEngine;
+
+// public class ParallaxEffect : MonoBehaviour
+// {
+//     private float length, startPos;
+//     public GameObject cam;
+//     public float parallaxEffect;
+
+//     void Start()
+//     {
+//         startPos = transform.position.x;
+//         length = GetComponent<SpriteRenderer>().bounds.size.x;
+//     }
+
+//     void Update()
+//     {
+//         float temp = (cam.transform.position.x * (1 - parallaxEffect));
+//         float dist = (cam.transform.position.x * parallaxEffect);
+
+//         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+//         if (temp > startPos + length) startPos += length;
+//         else if (temp < startPos - length) startPos -= length;
+//     }
+// }
 using UnityEngine;
 
 public class ParallaxEffect : MonoBehaviour
 {
-    private float length, startPos;
-    public GameObject cam;
-    public float parallaxEffect;
+    private float startPos;
+    private float length;
+    public GameObject cam; // Reference to the main camera
+    public float parallaxEffect; // Factor to control the parallax effect
 
     void Start()
     {
-        startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position.x; // Initial position of the background
+        length = GetComponent<SpriteRenderer>().bounds.size.x; // Width of the background sprite
     }
 
-    void Update()
+    void LateUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float camPosX = cam.transform.position.x; // Current camera position on x-axis
+        float dist = camPosX * parallaxEffect; // Distance to move based on the parallax effect
 
+        // Update the position of the background (only along the x-axis)
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
 
-        if (temp > startPos + length) startPos += length;
-        else if (temp < startPos - length) startPos -= length;
+        // Calculate temporary position to check looping
+        float temp = camPosX * (1 - parallaxEffect);
+
+        // Adjust the start position to create a looping effect
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
     }
 }
+
+
--- My project/Assets/Scripts/control.cs
+++ My project/Assets/Scripts/control.cs
@@ -34,9 +34,6 @@
 //         transform.position = smoothedPosition;
 //     }
 // }
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Control : MonoBehaviour
@@ -44,11 +41,12 @@ public class Control : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     public float smoothSpeed = 0.125f;
+    public float verticalSmoothSpeed = 0.05f; // Separate smooth speed for vertical movement
     public float minY = -10f; // Minimum y position
     public float maxY = 10f;  // Maximum y position
     private float orthographicHalfHeight;
+    private float currentYVelocity; // Velocity reference for smooth damping
 
-    // Start is called before the first frame update
     void Start()
     {
         if (target == null)
@@ -67,7 +65,6 @@ public class Control : MonoBehaviour
         orthographicHalfHeight = Camera.main.orthographicSize;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         if (target == null) return;
@@ -75,12 +72,14 @@ public class Control : MonoBehaviour
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
 
+        // Apply additional smoothing for vertical movement
+        float smoothedY = Mathf.SmoothDamp(transform.position.y, desiredPosition.y, ref currentYVelocity, verticalSmoothSpeed);
+
         // Clamp the y position within the specified range adjusted for the orthographic size
-        float clampedY = Mathf.Clamp(smoothedPosition.y, minY + orthographicHalfHeight, maxY - orthographicHalfHeight);
+        float clampedY = Mathf.Clamp(smoothedY, minY + orthographicHalfHeight, maxY - orthographicHalfHeight);
         smoothedPosition.y = clampedY;
 
         transform.position = smoothedPosition;
     }
 }
-
 
