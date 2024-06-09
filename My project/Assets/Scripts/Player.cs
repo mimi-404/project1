72293ccc35d5a3d48e481c3f1784b6864c0f60e3
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-09 20:32:33 +0545

    car db, car switching

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,36 +1,103 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Player : MonoBehaviour
+// {   
+//     public CarDatabase carDB;
+
+//     public SpriteRenderer artworkSprite;
+//     private int selectedOption = 0;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//           if (!PlayerPrefs.HasKey("selectedOption"))
+//         {
+//            selectedOption = 0;
+//         }
+//         else
+//         {
+//            Load();
+//         }
+//         UpdateCar(selectedOption);
+//     }
+
+
+//     private void UpdateCar(int selectedOption)
+//     {
+//         Car car = carDB.GetCar(selectedOption);  // Corrected typo here
+//         artworkSprite.sprite = car.carSprite;
+//     }
+//     private void Load()
+//     {
+//        selectedOption = PlayerPrefs.GetInt("selectedOption");
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{   
-    public CarDatabase carDB;
+{
+   public CarDatabase carDB;
+   public SpriteRenderer artworkSprite;
+   private int selectedOption = 0;
 
-    public SpriteRenderer artworkSprite;
-    private int selectedOption = 0;
+   void Start()
+   {
+      Debug.Log("Start method called");
 
-    // Start is called before the first frame update
-    void Start()
-    {
-          if (!PlayerPrefs.HasKey("selectedOption"))
-        {
-           selectedOption = 0;
-        }
-        else
-        {
-           Load();
-        }
-        UpdateCar(selectedOption);
-    }
+      if (!PlayerPrefs.HasKey("selectedOption"))
+      {
+         selectedOption = 0;
+         Debug.Log("No selectedOption key in PlayerPrefs, defaulting to 0");
+      }
+      else
+      {
+         Load();
+         Debug.Log("Loaded selectedOption: " + selectedOption);
+      }
 
- 
-    private void UpdateCar(int selectedOption)
-    {
-        Car car = carDB.GetCar(selectedOption);  // Corrected typo here
-        artworkSprite.sprite = car.carSprite;
-    }
-    private void Load()
-    {
-       selectedOption = PlayerPrefs.GetInt("selectedOption");
-    }
+      UpdateCar(selectedOption);
+   }
+
+   private void UpdateCar(int selectedOption)
+   {
+      Debug.Log("UpdateCar method called with selectedOption: " + selectedOption);
+
+      if (carDB == null)
+      {
+         Debug.LogError("carDB is not assigned in the Inspector.");
+         return;
+      }
+
+      Car car = carDB.GetCar(selectedOption);
+      if (car == null)
+      {
+         Debug.LogError("No car found for selectedOption: " + selectedOption);
+         return;
+      }
+
+      if (car.carSprite == null)
+      {
+         Debug.LogError("Car sprite is null for selectedOption: " + selectedOption);
+         return;
+      }
+
+      if (artworkSprite == null)
+      {
+         Debug.LogError("artworkSprite is not assigned in the Inspector.");
+         return;
+      }
+
+      artworkSprite.sprite = car.carSprite;
+      Debug.Log("Updated artworkSprite with car sprite for selectedOption: " + selectedOption);
+   }
+
+   private void Load()
+   {
+      selectedOption = PlayerPrefs.GetInt("selectedOption");
+      Debug.Log("Loaded selectedOption from PlayerPrefs: " + selectedOption);
+   }
 }
--- My project/Assets/Scripts/gamemanager.cs
+++ My project/Assets/Scripts/gamemanager.cs
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.UI;
-
-// public class gamemanager : MonoBehaviour
-// {
-//     public static gamemanager instance;
-
-//     [SerializeField] private GameObject _gameOverCanvas;
-//     [SerializeField] private Canvas mainGameCanvas;
-//     [SerializeField] private GameObject imageCanvas; // Add this line to reference the Image Canvas
-
-//     private void Awake()
-//     {
-//         if (instance == null)
-//         {
-//             instance = this;
-//         }
-
-//         Time.timeScale = 1f;
-//     }
-
-//     public void GameOver()
-//     {
-//         _gameOverCanvas.SetActive(true);
-//         if (imageCanvas != null)
-//         {
-//             imageCanvas.SetActive(true); // Enable the Image Canvas when the game is over
-//         }
-//         else
-//         {
-//             Debug.LogError("Image Canvas not assigned in gamemanager script.");
-//         }
-//         DisableCanvasUI(mainGameCanvas);
-//         Time.timeScale = 0f;
-//     }
-
-//     public void RestartGame()
-//     {
-//         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-//     }
-
-//     private void DisableCanvasUI(Canvas canvas)
-//     {
-//         if (canvas != null)
-//         {
-//             Button[] buttons = canvas.GetComponentsInChildren<Button>();
-//             foreach (Button button in buttons)
-//             {
-//                 button.interactable = false;
-//             }
-
-//             Selectable[] selectables = canvas.GetComponentsInChildren<Selectable>();
-//             foreach (Selectable selectable in selectables)
-//             {
-//                 selectable.interactable = false;
-//             }
-//         }
-//         else
-//         {
-//             Debug.LogError("Main Game Canvas not assigned in gamemanager script.");
-//         }
-//     }
-// }
-
-=======
->>>>>>> a69b22e9071ecb3ecbd8a637ab1b54dd6923dd9b
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -78,22 +8,7 @@ public class gamemanager : MonoBehaviour
 
     [SerializeField] private GameObject _gameOverCanvas;
     [SerializeField] private Canvas mainGameCanvas;
-<<<<<<< HEAD
-    [SerializeField] private GameObject imageCanvas; // Add this line to reference the Image Canvas
-
-    private bool isGamePaused = false;
-    public Button pauseButton; // Reference to the UI button for pausing
-    public Button playButton; // Reference to the UI button for resuming
-
-    void Start()
-    {
-        controlScript = Camera.main.GetComponent<Control>(); // Get Control script attached to main camera
-        // Other initialization...
-        UpdatePauseButton(); // Initially update button visibility
-    }
-=======
     [SerializeField] private GameObject imageCanvas; // Reference to the Image Canvas
->>>>>>> a69b22e9071ecb3ecbd8a637ab1b54dd6923dd9b
 
     private void Awake()
     {
@@ -117,28 +32,12 @@ public class gamemanager : MonoBehaviour
             Debug.LogError("Image Canvas not assigned in gamemanager script.");
         }
         DisableCanvasUI(mainGameCanvas);
-        Time.timeScale = 0f; // Pause the game
-        isGamePaused = true;
-        UpdatePauseButton(); // Update button visibility
+        Time.timeScale = 0f;
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    public void PauseGame()
-    {
-        Time.timeScale = 0f; // Pause the game
-        isGamePaused = true;
-        UpdatePauseButton(); // Update button visibility
-    }
-
-    public void ResumeGame()
-    {
-        Time.timeScale = 1f; // Resume the game
-        isGamePaused = false;
-        UpdatePauseButton(); // Update button visibility
     }
 
     private void DisableCanvasUI(Canvas canvas)
@@ -160,29 +59,6 @@ public class gamemanager : MonoBehaviour
         else
         {
             Debug.LogError("Main Game Canvas not assigned in gamemanager script.");
-        }
-    }
-
-    private void UpdatePauseButton()
-    {
-        if (pauseButton != null && playButton != null)
-        {
-            if (isGamePaused)
-            {
-                // If game is paused, show play button and hide pause button
-                playButton.gameObject.SetActive(true);
-                pauseButton.gameObject.SetActive(false);
-            }
-            else
-            {
-                // If game is not paused, show pause button and hide play button
-                pauseButton.gameObject.SetActive(true);
-                playButton.gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            Debug.LogError("Pause Button or Play Button not assigned in gamemanager script.");
         }
     }
 }
