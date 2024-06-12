339ff3e1cf5a2c7407cc8e441da56bbcfd076780
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-12 16:20:54 +0545

    unnecessary lines omitted

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -1,26 +1,4 @@
 
-// using UnityEngine;
-
-// public class CameraFollow : MonoBehaviour
-// {
-//     public Transform target;
-//     public DriveCar vehicle;
- 
-//     public Vector3 offset = new Vector3(10f, 0f, 0f);
-
-//     private void LateUpdate()
-//     {   
-//         if (target == null)
-//         {
-//             Debug.LogError("Target is not set");
-//             return;
-//         }
-//         float smoothSpeed = vehicle.GetSpeed() * 1.2f;
-//         Vector3 desiredPosition = target.position + offset;
-//         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-//         transform.position = smoothedPosition;
-//     }
-// }
 using UnityEngine;
 
 public class FollowCar : MonoBehaviour
