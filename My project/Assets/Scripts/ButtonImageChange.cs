74bf45296704cc7b2f3a6712bc2c56c2d6e74aa2
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-13 10:24:44 +0545

    engine start audio

--- My project/Assets/Scripts/ButtonImageChange.cs
+++ My project/Assets/Scripts/ButtonImageChange.cs
@@ -4,18 +4,24 @@ using UnityEngine.EventSystems;
 
 public class ButtonImageChange : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    public Image buttonImage; 
+    public Image buttonImage;
     public Sprite normalImage;
-    public Sprite pressedImage; 
+    public Sprite pressedImage;
 
-    
+    AudioManager audioManager;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
-        buttonImage.sprite = pressedImage; 
+        audioManager.PlaySFX(audioManager.engine);
+        buttonImage.sprite = pressedImage;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        buttonImage.sprite = normalImage; 
+        //audioManager.PlaySFX(audioManager.engine);
+        buttonImage.sprite = normalImage;
     }
 }
--- My project/Assets/Scripts/backto menu.cs
+++ My project/Assets/Scripts/backto menu.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class backtomenu : MonoBehaviour
 {
+    AudioManager audioManager;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
     public void LoadMainMenu()
     {
+        audioManager.PlaySFX(audioManager.buttonClick);
         SceneManager.LoadScene("main");
     }
 }
