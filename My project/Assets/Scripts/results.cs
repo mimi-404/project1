2dc0766d0384c0780969ec924c19f37244fed0fe
mimi-404 <sm03231722@student.ku.edu.np>
2024-06-13 12:57:49 +0545

    scoreboard data display and game pause after game over

---
+++ My project/Assets/Scripts/results.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Results : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _finalScoreText;
+    [SerializeField] private TextMeshProUGUI _highscoreText;
+    [SerializeField] private TextMeshProUGUI _coinText;
+
+    private void Start()
+    {
+        DisplayEndGameStats();
+    }
+
+    private void DisplayEndGameStats()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        float highscore = 0f;
+        string highscoreKey = "";
+
+        if (currentScene.name == "rushrally1")
+        {
+            highscoreKey = "ClassicHigh_" + currentScene.buildIndex;
+        }
+        else if (currentScene.name == "TimeChallenge")
+        {
+            highscoreKey = "TimeHigh_" + currentScene.buildIndex;
+        }
+
+        highscore = PlayerPrefs.GetFloat(highscoreKey, 0f);
+        int coins = PlayerPrefs.GetInt("CoinCount", 0);
+
+        _highscoreText.text = $"High Score: {highscore:F0}m";
+        _coinText.text = $"Coins: {coins}";
+
+        float finalScore = DistanceCovered.instance.GetCurrentDistance();
+        _finalScoreText.text = $"Score: {finalScore:F0}m";
+    }
+}
