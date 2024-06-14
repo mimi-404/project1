5ddac907c6fe8f0c753adc5cfca57e0b2e949e6a
sushma <sushmaacharya984@gmail.com>
2024-06-14 09:37:32 +0545

    coinmainmenu

---
+++ My project/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using TMPro;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI coinCountText;
+
+    private const string CoinPlayerPrefsKey = "CoinCount";
+
+    private void Start()
+    {
+        DisplayCoinCount();
+    }
+
+    private void DisplayCoinCount()
+    {
+        int coinCount = PlayerPrefs.GetInt(CoinPlayerPrefsKey, 0);
+        coinCountText.text = "Coins: " + coinCount.ToString();
+    }
+}
